eda9cea386dd567e04d73cda0b0c5c4b7544282d
Chien-Sheng Yang <39869317+paul555111@users.noreply.github.com>
2020-05-05 08:23:03 -0700

    Update preaggregator.py

--- preaggregator.py
+++ preaggregator.py
@@ -7,18 +7,20 @@ def task(filelist, pathin, pathout):
     job_id = filelist[0].partition('outscore')[0]
     job_id = job_id[3:]
     
-    # Load Dictionary of recieved results
+    ###TO DO: Load Dictionary of recieved results from the server##############
     dict_path = '/Job_Dictionary/job_dictionary.txt' # to de defined in advacne
     with open(dict_path,'r') as inf:
         job_dict = eval(inf.read())
+    ###########################################################################
     
     #Parameters
     M = 2 # Number of data-batches
     
-    #Update Dictionanry
+    ## TO DO: Update Dictionanry in the server#################################
     job_dict[job_id].append(filelist[0])
     with open('./Job_Dictionary/job_dictionary.txt', 'w') as outfile:
         json.dump(job_dict, outfile)
+    ###########################################################################
     
     #Check if number of received results for the same job is equal to M
     if len(job_dict[job_id]) == M:
