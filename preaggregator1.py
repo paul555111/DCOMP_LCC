21b68eb7b2437d3f65770497ec666b7f4448ca68
Chien-Sheng Yang <39869317+paul555111@users.noreply.github.com>
2020-05-06 21:33:02 -0700

    Update and rename preaggregator.py to preaggregator1.py

--- preaggregator1.py
+++ preaggregator1.py
@@ -7,7 +7,8 @@ import json
 
 
 def task(filelist, pathin, pathout):     
-    job_id = filelist[0].partition('outscore')[0]
+    snapshot_time = filelist[0].partition('_')[2].partition('_')[2].partition('_')[2].partition('.')[0]  #store the data&time info 
+    job_id = filelist[0].partition('_')[2].partition('_')[2].partition('_')[0]
     job_id = job_id[3:]
     
     
@@ -32,11 +33,13 @@ def task(filelist, pathin, pathout):
     
     #Check if number of received results for the same job is equal to M
     if len(job_dict[job_id]) == M:
+        print('Receive enough results for job '+job_id)
         for i in range(M):
             En_Image_Batch = np.loadtxt(os.path.join(pathin, (job_dict[job_id])[i]), delimiter=',')
-            np.savetxt(os.path.join(pathout,(job_dict[job_id])[i].replace('outscore','outpreagg')), En_Image_Batch, delimiter=',')
-
+            np.savetxt(os.path.join(pathout,'preaggregator1_lccdecoder1_'+filelist[0].partition('_')[0]+'_job'+job_id+'_'+snapshot_time+'.csv'), En_Image_Batch, delimiter=',')
+    else:
+        print('Not receive enough results for job '+job_id)
             
 if __name__ == '__main__': ##THIS IS FOR TESTING - DO THIS
-    filelist= ['job1outscore1_20200424.csv'] 
+    filelist= ['score1a_preaggregator1_job1_20200424.csv'] 
     task(filelist,'./Enc_Results', './Agg_Results')  
